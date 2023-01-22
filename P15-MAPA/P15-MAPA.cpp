683bb1b045cb541b874f35261b1ba93cc74d2f30
daniel-fdez <113612097+daniel-fdez@users.noreply.github.com>
2023-01-22 12:43:59 +0100

    Update P15-MAPA.cpp

--- P15-MAPA/P15-MAPA.cpp
+++ P15-MAPA/P15-MAPA.cpp
@@ -1,5 +1,3 @@
-// Autor/a: Daniel Fernández Ortiz
-// email: danief17@ucm.es
 // Compilador y S.O. utilizado: VS 2019
 // Nombre del problema: Máxima cota de un mapa topográfico
 // Comentario general sobre la solución: Número de casos de prueba ilimitados
