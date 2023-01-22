24ffa8afb69d6eb0bbecdd9c70f3749943311905
daniel-fdez <113612097+daniel-fdez@users.noreply.github.com>
2023-01-22 12:45:24 +0100

    Update P16-PAPA_NOEL.cpp

--- P16-PAPA_NOEL/P16-PAPA_NOEL.cpp
+++ P16-PAPA_NOEL/P16-PAPA_NOEL.cpp
@@ -1,5 +1,3 @@
-// Autor/a: Daniel Fernández Ortiz
-// email: danief17@ucm.es
 // Compilador y S.O. utilizado: VS 2019
 // Nombre del problema: P16-PAPA NOEL
 // Comentario general sobre la solución: Número de casos ilimitado
