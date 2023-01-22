26500cb7b631c2b061cf68b28320e37dfe0d61ff
daniel-fdez <113612097+daniel-fdez@users.noreply.github.com>
2023-01-22 12:52:34 +0100

    Update P60-MATRICES.cpp

--- P60-MATRICES/P60-MATRICES.cpp
+++ P60-MATRICES/P60-MATRICES.cpp
@@ -1,7 +1,4 @@
-//RE
-////Autor/a: Daniel Fernández Ortiz
-////email: danief17@ucm.es
-////Compilador y S.O. utilizado: VS 2019
+////RE
 ////Nombre del problema: Diversión con matrices Parte 1
 ////Comentario general sobre la solución: Casos de prueba ilimitados acotados por un caso de prueba especial
 //
@@ -194,8 +191,6 @@
 //}
 
 // WA
-//Autor/a: Daniel Fernández Ortiz
-//email: danief17@ucm.es
 //Compilador y S.O. utilizado: VS 2019
 //Nombre del problema: Diversión con matrices Parte 1
 //Comentario general sobre la solución: Casos de prueba ilimitados acotados por un caso de prueba especial
